a724a04ecdb1523fae280bbd4c1e2b7a603c010c
Unknown <ocebeki@gmail.com>
2018-05-28 19:40:32 +0100

    Ready

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $('.multi-item-carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        } else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
+    
+});
